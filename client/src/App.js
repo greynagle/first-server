ec4ac31cc705518b698a3a97d5a89ddc35f70224
Grey <grey.nagle@gmail.com>
2019-09-03 21:20:55 -0400

    inputs, cleaned table

--- client/src/App.js
+++ client/src/App.js
@@ -1,146 +1,132 @@
 import React, { Component } from 'react';
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import propTypes from 'prop-types'
+import { Input, TextField, Paper } from '@material-ui/core';
 
-// const useStyles = makeStyles(theme => ({
-//     root: {
-//       width: '100%',
-//     },
-//     paper: {
-//       marginTop: theme.spacing(3),
-//       width: '100%',
-//       overflowX: 'auto',
-//       marginBottom: theme.spacing(2),
-//     },
-//     table: {
-//       minWidth: 650,
-//     },
-//   }));
+const styles = theme => ({
+  paper: {
+    width: '85%',
+    marginTop: '5%',
+    overflow: 'auto',
+  }
+})
 
-const TblTemplate = (props) => {
-  console.log("props")
-  console.log(props.tblGuest)
-  console.log(Object.values(props)[0])
-  // const classes = useStyles();
+const GuestTable = ({guestList}) => {
+  const classes = styles()
 
   return (
-    <div >
-      {/* error */}
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Guests</TableCell>
-              <TableCell align="right">First Name</TableCell>
-              <TableCell align="right">Last Name</TableCell>
-              <TableCell align="right">Status</TableCell>
-              <TableCell align="right">+1?</TableCell>
-              <TableCell align="right">Lock</TableCell>
+    <Paper className={classes.paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Guests</TableCell>
+            <TableCell align="right">First Name</TableCell>
+            <TableCell align="right">Last Name</TableCell>
+            <TableCell align="right">Status</TableCell>
+            <TableCell align="right">+1?</TableCell>
+            <TableCell align="right">Lock</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {guestList.map(row => (
+            <TableRow key={row.id}>
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="right">{row.firstName}</TableCell>
+              <TableCell align="right">{row.lastName}</TableCell>
+              <TableCell align="right">{row.status}</TableCell>
+              <TableCell align="right">{row.plusOne}</TableCell>
+              <TableCell align="right">{row.isLocked}</TableCell>
             </TableRow>
-          </TableHead>
-          <TableBody>
-            {props.tblGuest.map(row => (
-              <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell align="right">{row.firstName}</TableCell>
-                <TableCell align="right">{row.lastName}</TableCell>
-                <TableCell align="right">{row.inviteStatus}</TableCell>
-                <TableCell align="right">{row.plusOne}</TableCell>
-                <TableCell align="right">{row.isLocked}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-
-    </div>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   ) 
 }
-TblTemplate.propTypes = {tblGuest:propTypes.array.isRequired}
+GuestTable.propTypes = {guestList:propTypes.array.isRequired}
+
+const TextInput = (label, input) => {
+  return (
+    <Input
+      placeholder={label}
+      label={label}
+      inputProps={{
+        'aria-label': 'description',
+      }}
+      {...input}
+    />
+  )
+}
+TextInput.propTypes = {}
 
 class App extends Component {
   state = {
     response: 'Default response',
-    post: '',
+    firstName: '',
+    lastName: '',
     responseToPost: '',
-    tblGuest: []
+    guestList: []
   };
-  
   
   componentDidMount() {
     this.callApi()
-      .then(res => {
-        const tblGuestByID = res.reduce((acc, cur) => {
-          return [
-            ...acc, {
-              id: cur.id,
-              firstName: cur.firstName,
-              lastName: cur.lastName,
-              inviteStatus: cur.status,
-              plusOne:cur.plusOne,
-              isLocked:cur.isLocked,
-            }
-          ]
-        }, []);
-        console.log("tblGuestByID")
-        console.log(tblGuestByID)
-        this.setState({tblGuest: tblGuestByID});
-      })
+      .then(guestList => {this.setState({guestList})})
       .catch(err => console.log(err));  
   }
   
   callApi = async () => {
-    // return new Promise((resolve, reject) => {
-    //   fetch('/api/hello')
-    //   .then(res => resolve(res))
-    //   .catch(e => reject(e))
-    // });
     const response = await fetch('/api/hello');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
-    console.log(body)
     
     return body;
   };
   
   handleSubmit = async e => {
-    e.preventDefault();
-    console.log('submitted')
+    // e.preventDefault();
     const response = await fetch('/api/world', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({firstName: this.state.post, lastName: "test", plusOne: 1, isInvited: 1}),
+      body: JSON.stringify({firstName: this.state.firstName, lastName: this.state.lastName}),
     });
     const body = await response.text();
     
     this.setState({ responseToPost: body });
   };
 
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+  
   render() {
     return (
       <div>
-        <div>
-          <TblTemplate tblGuest={this.state.tblGuest}/>
+        <div>          
+          <GuestTable 
+            guestList={this.state.guestList}
+          />
         </div>
         <div className='App'>
-          <p>Input Guest information</p>
+          <p>Guest Information</p>
           <form onSubmit={this.handleSubmit}>
-            <p>
-              <strong>Post to Server:</strong>
-            </p>
-            <input
-              type="text"
-              value={this.state.post}
-              onChange={e => this.setState({ post: e.target.value })}
+            <TextField
+              label="First Name"
+              name="firstName"
+              onChange={this.handleChange}
+            />
+            <TextField
+              label="Last Name"
+              name="lastName"
+              onChange={this.handleChange}
             />
             <button type="submit">Submit</button>
           </form>
--- server.js
+++ server.js
@@ -20,8 +20,6 @@ db.connect( (err) => {
 });
 global.db = db;
 
-console.log(port);
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -33,26 +31,21 @@ app.get('/api/hello', (req, res) => {
       res.send({bad:'error'});
       return 
     }
-    console.log(result);
     res.send(result);
   })
-  
-  // res.send({ express: 'Hello From Express' });
 });
 
-app.post('/api/world', (req, res) => {
-  const query = `insert into tblguest (firstName,lastName,plusOne,isLocked) values ("${req.body.firstName}","${req.body.lastName}",${req.body.plusOne},${req.body.isInvited}) `;
-  console.log(query);
+app.post('/api/world', ({body}, res) => {
+  console.log(body)
+  const query = `insert into tblguest (firstName,lastName,plusOne,isLocked) values ("${body.firstName}","${body.lastName}",1,1)`;
   db.query(query,(err, result) => {
     if (err) {
       console.log('bad')
       res.send({bad:'error'});
       return 
     }
-    console.log(result);
     res.send({result});
   })
-  // res.send({ express: 'Hello From Express' });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
